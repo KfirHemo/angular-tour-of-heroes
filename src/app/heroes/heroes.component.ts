8940ebc2853f687be88e57ef3135b3623cdb0aec
KFIR HEMO <kfirhemo@gmail.com>
2023-08-13 14:48:43 +0300

    TOH first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent {
+  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  onSelect(hero:Hero): void{
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
