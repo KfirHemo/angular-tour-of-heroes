2516582300cf532ac375b34683696a7e5fe5883c
KFIR HEMO <kfirhemo@gmail.com>
2023-08-14 14:22:04 +0300

    Finished code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { Hero } from '../Hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,19 +9,29 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
-  constructor(private heroService: HeroService) {}
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
-  onSelect(hero:Hero): void{
-    this.selectedHero = hero;
-  }
+  constructor(private heroService: HeroService) { }
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
   ngOnInit(): void {
     this.getHeroes();
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
